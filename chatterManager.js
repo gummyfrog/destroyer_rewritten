567e1e9001a616d974b3ed3fb6d716e9ecac2705
Neil <neilsemail@me.com>
2019-08-12 22:46:29 -0400

    NLP beta

--- chatterManager.js
+++ chatterManager.js
@@ -1,27 +1,45 @@
 var Necessary = require('./necessary.js');
-var ElizaBot = require('./elizabot/elizabot.js');
+var { NlpManager } = require('node-nlp');
+
 
 module.exports = class chatter extends Necessary {
 
 	constructor() {
 		super();
-		this.eliza = new ElizaBot();
+		this.manager = new NlpManager({ languages: ['en'] });		
 		this.conversingStatus = false;
+
+
+		this.manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+		this.manager.addDocument('en', 'hi', 'greetings.hello');	
+		this.manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+		this.manager.addAnswer('en', 'greetings.bye', 'see you soon!');
 	}
 
 	start(message) {
 		this.conversingStatus = true;
-		message.channel.send(this.eliza.getInitial());
 	}
 
 	ask(message) {
-		console.log("chask")
-		message.channel.send(this.eliza.transform(message.content));
+		(async() => {
+		    await this.manager.train();
+		    this.manager.save();
+		    const response = await this.manager.process('en', message.content);
+		    console.log(response);
+
+		    if(response.answer) {
+		    	message.channel.send(response.answer)
+		    }
+
+		    this.manager.addDocument('en', message.content, message.content.split(' ')[0])
+		    this.manager.addAnswer('en', message.content.split(' ')[0], message.content)
+
+		    
+		})();
 	}
 
 	stop(message) {
 		this.conversingStatus = false;
-		message.channel.send(this.eliza.getFinal())
 	}
 
 }
