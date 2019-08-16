062c98d67b178508bffb828fb8aa400fdd9e0951
Neil <neilsemail@me.com>
2019-08-15 22:30:19 -0400

    fix

--- commands/reference.js
+++ commands/reference.js
@@ -1,10 +1,11 @@
 module.exports = {
-	name: 'Quote',
-	description: 'Saves a message.',
+	name: 'Reference',
+	description: 'Clones a message for reference.',
 	keys: ['r', 'reference'],
 	required: ["quotes"],
 	execute(message, args, managers = {}) {
 		managers.quotes.reference(message, args);
+		message.delete();
 	},
 };
 
